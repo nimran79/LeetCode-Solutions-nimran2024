d5fd84a5e6df57b6aec9641d9a6bf87cfbc230c2
Navid Mohammad Imran <55009162+nimran79@users.noreply.github.com>
2024-02-26 04:59:25 -0500

    Solution using Sets

---
+++ 217-contains-duplicate/contains-duplicate-v2.py
@@ -0,0 +1,4 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # using Set's inherent property to store unique elements
+        return len(set(nums)) != len(nums)
