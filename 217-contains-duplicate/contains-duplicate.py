db6c177cd5769459641c93b54c7f2d886d1648b2
Navid Mohammad Imran <55009162+nimran79@users.noreply.github.com>
2024-02-26 04:56:06 -0500

    Time: 439 ms (29.23%) | Memory: 34.6 MB (16.43%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,5 +1,10 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # using Set's inherent property to store unique elements
-        return len(set(nums)) != len(nums)
+        # Using a dictionary 'seen' as a hash table
+        seen = {}
+        for num in nums:
+            if num in seen:
+                return True
+            seen[num] = True # Mark the number as seen
+        return False
         
