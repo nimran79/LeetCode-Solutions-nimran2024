36956454d9edd5b30790f2b7a483d7c1f90aafa8
Navid Mohammad Imran <55009162+nimran79@users.noreply.github.com>
2024-03-21 18:02:49 -0400

    Time: 47 ms (6.53%) | Memory: 17.7 MB (33.94%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -10,10 +10,16 @@ class Solution:
         next_node  = None
         
         while curr_node is not None:
+            # Save the next node
             next_node = curr_node.next
+
+            # Reverse the link so that current_node.next points to the node before it
             curr_node.next = prev_node
             prev_node = curr_node
+
+            # Move to the next node in the original list
             curr_node = next_node
 
+        # prev_node now points to the head of the reversed list
         return prev_node
         
