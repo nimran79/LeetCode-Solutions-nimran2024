2a3558445ae95c2d99c74d614accd4d7afc046a8
Navid Mohammad Imran <55009162+nimran79@users.noreply.github.com>
2024-03-22 10:45:03 -0400

    Time: 32 ms (86.00%) | Memory: 16.5 MB (49.62%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        c=nums.count(val)
+
+        while c!=0:
+            nums.remove(val)
+            c=c-1
+        return len(nums)
