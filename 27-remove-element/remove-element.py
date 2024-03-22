5301e6eba51a8f5dd88fddff5b3941870ef73364
Navid Mohammad Imran <55009162+nimran79@users.noreply.github.com>
2024-03-22 10:49:41 -0400

    Time: 35 ms (73.89%) | Memory: 16.5 MB (93.24%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,8 +1,19 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        c=nums.count(val)
+        # Since the order of the elements doesn't matter after removal, 
+        # you can simply overwrite elements that equal val with elements that do not
+        
+        # Initialize a new index for the updated list without the value 'val'
+        new_length = 0 
 
-        while c!=0:
-            nums.remove(val)
-            c=c-1
-        return len(nums)
+        # Iterate over each number in 'nums' to put valid elements at the front of 'nums'
+        for number in nums:
+            # If the current number is not the value to remove, update the list
+            if number != val:
+                # Assign the number to the new index location
+                nums[new_length] = number
+                # Increment the new length to move to the next index
+                new_length += 1
+
+        # Return the new length of the list after all removals are completed
+        return new_length
