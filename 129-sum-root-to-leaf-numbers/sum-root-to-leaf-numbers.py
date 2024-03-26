c8c4ed77795563ae7471b07152d5b8867f79da26
Navid Mohammad Imran <55009162+nimran79@users.noreply.github.com>
2024-03-26 07:46:18 -0400

    Time: 41 ms (18.23%) | Memory: 16.5 MB (26.38%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        # Helper function to perform depth-first search.
+        def dfs(node, num_sum):
+            # If the current node is None, we've reached a leaf or the tree is empty.
+            if not node:
+                return 0
+          
+            # Update the sum for the current path: 
+            # previous sum times 10 plus the current node's value since digits are 0~9.
+            num_sum = num_sum * 10 + node.val
+          
+            # If we're at a leaf node, return the current sum.
+            if not node.left and not node.right:
+                return num_sum
+          
+            # Continue the depth-first search on left and right subtrees, adding their sums.
+            return dfs(node.left, num_sum) + dfs(node.right, num_sum)
+      
+        # Start the depth-first search with the root node and an initial sum of 0.
+        return dfs(root, 0)
+        
